1248b8cebf63e406286713a2e4328f87e2ab7e36
hollyjelly <skguswl8038@naver.com>
2022-05-12 15:04:38 +0900

    css html jquery commit

--- js/yamahaPc_index.js
+++ js/yamahaPc_index.js
@@ -8,6 +8,28 @@ $(function(){
     var md = $('div.cntWrap.cntWrap03 div.article>div.image');
     var gray = $('div.cntWrap.cntWrap03 div.article');
     
+    var hdr = $('div.hdrWrap');
+    var hdbar = $('div.hdrWrap>div.header>div.hdrTop>div.btn');
+    var mnbar = $('div.navBox>div.top>div.btn');
+    var mn = $('div.navBox');
+    hdbar.click(function(){
+        mn.css({width:'100vw'});
+    });
+
+    mnbar.click(function(){
+        mn.css({width:'0'});
+    });
+
+    $(window).scroll(function(){
+        var scro = $(this).scrollTop();
+        if(scro>0){
+            hdr.css({backgroundColor:'#000',width:'100vw',margin:'0 auto'});
+        }else{
+            hdr.css({
+                background:'rgba(0,0,0,0.6)',margin:'0 auto'
+            });
+        }
+    });
     $(window).resize(function(){
 
         gray.eq(1).css({filter:'grayscale(1)'})
